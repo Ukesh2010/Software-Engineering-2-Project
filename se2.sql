9208053480dabe3484bac5d040ee1a5a63651ade
Ukesh2010 <ukesh2010@gmail.com>
2016-01-03 21:05:35 +0545

    version 0.1

--- se2.sql
+++ se2.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 15, 2015 at 06:58 PM
+-- Generation Time: Jan 03, 2016 at 04:17 PM
 -- Server version: 5.6.25
 -- PHP Version: 5.6.11
 
@@ -28,7 +28,12 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `counseller` (
   `counseller_id` int(11) NOT NULL,
-  `counseller_name` varchar(100) DEFAULT NULL
+  `counseller_username` varchar(100) DEFAULT NULL,
+  `counseller_password` varchar(100) NOT NULL,
+  `counseller_fullname` varchar(100) NOT NULL,
+  `counseller_address` varchar(100) NOT NULL,
+  `counseller_email` varchar(100) NOT NULL,
+  `counseller_phone_no` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -38,7 +43,6 @@ CREATE TABLE IF NOT EXISTS `counseller` (
 --
 
 CREATE TABLE IF NOT EXISTS `followup` (
-  `counseller_id` int(11) DEFAULT NULL,
   `lead_id` int(11) DEFAULT NULL,
   `followup_date` date DEFAULT NULL,
   `feedback` varchar(1000) DEFAULT NULL
@@ -52,7 +56,8 @@ CREATE TABLE IF NOT EXISTS `followup` (
 
 CREATE TABLE IF NOT EXISTS `intake` (
   `intake_id` int(11) NOT NULL,
-  `intake_start_date` date DEFAULT NULL
+  `intake_start_date` date DEFAULT NULL,
+  `intake_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -66,12 +71,21 @@ CREATE TABLE IF NOT EXISTS `lead` (
   `lead_first_name` varchar(100) DEFAULT NULL,
   `lead_middle_name` varchar(100) DEFAULT NULL,
   `lead_last_name` varchar(100) DEFAULT NULL,
-  `lead_adddress` varchar(100) DEFAULT NULL,
+  `lead_address` varchar(100) DEFAULT NULL,
   `lead_mobile_no` varchar(100) DEFAULT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  `isStudent` tinyint(4) DEFAULT NULL,
-  `intake_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `status` varchar(50) NOT NULL DEFAULT 'active',
+  `isStudent` tinyint(4) NOT NULL DEFAULT '0',
+  `intake_id` int(11) DEFAULT NULL,
+  `next_followup_date` date DEFAULT NULL,
+  `counseller_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lead`
+--
+
+INSERT INTO `lead` (`lead_id`, `lead_first_name`, `lead_middle_name`, `lead_last_name`, `lead_address`, `lead_mobile_no`, `status`, `isStudent`, `intake_id`, `next_followup_date`, `counseller_id`) VALUES
+(1, 'sfs', 'sdkf', 'sdf', 'sdf', 'ddsf', 'active', 0, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -95,6 +109,15 @@ ALTER TABLE `intake`
 ALTER TABLE `lead`
   ADD PRIMARY KEY (`lead_id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `lead`
+--
+ALTER TABLE `lead`
+  MODIFY `lead_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
